0501244219e140ca44bd359872f80c8b35fcc160
All3x <acprelle@gmail.com>
2022-08-16 11:51:43 -0300

    commit da aula 2, sobre enviar json, porem não funcionou

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //importando o express
+const { application } = require('express');
 const express = require('express')
 // definindo a variavel app como variavel do express
 const app = express();
@@ -6,6 +7,11 @@ const app = express();
 app.get('/', (req,res) =>{
     res.send('<h1>Coisas a fazer</h1>')
 })
+//adicionando uma resposta json (NÃO ESTÁ FUNCIONANDO)
+application.get('/json', (req, res) =>{
+    res.json({title: 'Tarefa X', done: true});
+})
+
 // setando a porta 3000 como porta do servidor e definindo uma mensagem de confimação bacaninha 
 app.listen(3000, () => {
     console.log('Ta tudo bacaninha');
