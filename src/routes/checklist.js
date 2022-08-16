eeca626eabbf514e21402bd4acbd6dee6ab0f388
All3x <acprelle@gmail.com>
2022-08-16 14:41:19 -0300

    parametros nas rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -12,4 +12,9 @@ router.post('/', (req , res) =>{
     res.status(200).json
     (req.body)
 })
-module.exports = router;
+//parametro na rota
+router.get('/:id', (req, res)=>{
+    console.log(req.params.id);
+    res.send(`ID: ${req.params.id}`)
+})
+module.exports = router; 
