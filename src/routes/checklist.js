fcb96cf907576993fc6fedda0f040035dd3be2a0
All3x <acprelle@gmail.com>
2022-08-17 15:05:42 -0300

    integrando a database com o app, e setando a rota post para criar uma task

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const checklist = require('../models/checklist');
 
 const router = express.Router();
+
+const Checklist = require('../models/checklist')
 
 router.get('', (req, res) =>{
     console.log('Olá');
     res.end();
 })
+
 // nova rota post
-router.post('/', (req , res) =>{
-    console.log(req.body);
-    res.status(200).json
-    (req.body)
+router.post('/', async (req , res) =>{
+    let {name} = req.body;
+    console.log(name);
+    //         OU
+    // console.log(req.body["name"]);
+    try{
+        let checklist = await Checklist.create({name})
+        res.status(200).json(checklist);
+    } catch (error){
+        res.status(422).json(error)
+    }
 })
+
 //parametro na rota
 router.get('/:id', (req, res)=>{
     console.log(req.params.id);
