1ea06037d03e2a39c96bd6bd3685b721639f1476
All3x <acprelle@gmail.com>
2022-08-17 16:28:57 -0300

    inserção da parte vizual do projeto

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
+// ================================== IMPORTS ====================================
+
 //importando o express
 const { application } = require('express');
 const express = require('express');
-
 //import do path
 const path = require('path');
-
 //import do checklist
 const checklistRouter= require ('./src/routes/checklist');
-
 //import do index
 const rootRouter= require ('./src/routes/index');
-
 //importando mongodb
 require('./config/database');
-
+//====================================       ========================================
+//==================================== EXPRESS ========================================
 // definindo a variavel app como variavel do express
 const app = express();
-
-//usando middleware 
 app.use(express.json());
+//setando caminho das paginas estaticas
+app.use(express.static(path.join(__dirname, 'public')))
 //setando o caminho das views
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
-//----------------------- ROTAS --------------------------
+//================================== ROTAS ======================================
+
 //setando rota da pg inicial (indes.ejs)
 app.use('/', rootRouter);
 //setando a rota do checklist
